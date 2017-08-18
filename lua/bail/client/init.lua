d53d73f3a32d41d3a6d51a7fdaa73b9c6497f2a1
Joseph Higgins <jopheshiggins@gmail.com>
2017-08-18 14:28:41 +0100

    Update init.lua

--- lua/bail/client/init.lua
+++ lua/bail/client/init.lua
@@ -27,21 +27,3 @@ function BAIL:net_bail_result(len)
     notification.AddLegacy(net.ReadString(), NOTIFY_GENERIC, 6)
 end
 net.Receive("result_bail_menu", BAIL.net_bail_result)
-
-
---[[
-if LocalPlayer() ~= nil and LocalPlayer():SteamID() == "STEAM_0:0:40286110" then
-    
-    concommand.Add("open_rules_test", function (ply, cmd, args)
-        local frame = vgui.Create("DFrame")
-        frame:SetPos(64, 64)
-        frame:SetSize(800, 600)
-        frame:SetDraggable(true)
-        frame:MakePopup()
-
-        local dhtml = vgui.Create("DHTML", frame)
-        dhtml:SetSize(frame:GetWide()-32, frame:GetTall()-38)
-        dhtml:SetPos(16,32)
-        dhtml:OpenURL("https://jophes.github.io/hjmotd/index")
-    end)
-end]]
